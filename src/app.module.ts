c42b87f138856385aefb47364b09617fffb0125a
CodeArtisT75 <pcstar7000@gmail.com>
2020-08-18 17:46:08 +0430

    add Roles guard to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { RoleModule } from './role/role.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './common/guards/roles.guard';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI), UserModule, AuthModule, RoleModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
