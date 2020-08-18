0719980395b0633c7465fcc302e822791ec6a7c7
CodeArtisT75 <pcstar7000@gmail.com>
2020-08-18 12:14:00 +0430

    add JWT strategy to Auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Post, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, HttpCode, Get } from '@nestjs/common';
 import { HttpResponse } from 'src/common/interfaces/http';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
+  @HttpCode(200)
   async login(@Req() req: Request): Promise<HttpResponse> {
+    return {
+      message: 'ok',
+      data: await this.authService.token(req.user),
+    };
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('user')
+  async getCurrentUser(@Req() req: Request): Promise<HttpResponse> {
     return {
       message: 'ok',
       data: req.user,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { AuthDTO } from './dto/auth.dto';
 import { User } from 'src/user/user.schema';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(userData: AuthDTO): Promise<User> {
     let user = await this.userService.findByUserName(userData.username);
@@ -22,5 +26,13 @@ export class AuthService {
     }
 
     return user;
+  }
+
+  async token(user: any): Promise<any> {
+    const payload = { id: user._id, username: user.username };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
